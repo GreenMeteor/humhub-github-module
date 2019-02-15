293e3e3dc988f66781331f2751b0f090751f476e
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-02-14 23:38:27 -0500

    Coding standards + PHP 7.2 Usage

--- Events.php
+++ Events.php
@@ -7,14 +7,14 @@ use yii\helpers\Url;
 use humhub\modules\github\widgets\GithubFrame;
 use humhub\models\Setting;
 
-class Events extends \yii\base\Object
+class Events extends \yii\base\BaseObject
 {
 
     public static function onAdminMenuInit(\yii\base\Event $event)
     {
         $event->sender->addItem([
             'label' => Yii::t('GithubModule.base', 'GitHub Settings'),
-            'url' => Url::toRoute('/Github/admin/index'),
+            'url' => Url::toRoute('/github/admin/index'),
             'group' => 'settings',
             'icon' => '<i class="fa fa-github"></i>',
             'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'github' && Yii::$app->controller->id == 'admin',
@@ -27,8 +27,8 @@ public static function addGithubFrame($event)
         if (Yii::$app->user->isGuest) {
             return;
         }
-        $event->sender->view->registerAssetBundle(Assets::className());
-        $event->sender->addWidget(GithubFrame::className(), [], [
+        $event->sender->view->registerAssetBundle(Assets::class);
+        $event->sender->addWidget(GithubFrame::class, [], [
             'sortOrder' => Setting::Get('timeout', 'github')
         ]);
     }

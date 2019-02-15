fbdd66fddbb0059728bb2a8af8b8bdcb797731e2
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-02-15 00:23:35 -0500

    Update GithubFrame.php

--- widgets/GithubFrame.php
+++ widgets/GithubFrame.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace humhub\modules\github\widgets;
 
+use Yii;
 use yii\base\Widget;
 
 class GithubFrame extends Widget
@@ -8,9 +10,12 @@ class GithubFrame extends Widget
 
     public $contentContainer;
 
+    /**
+     * @inheritdoc
+     */
     public function run()
     {
-
+        Yii::$app->getModule('github');
         return $this->render('githubframe', []);
     }
 }
